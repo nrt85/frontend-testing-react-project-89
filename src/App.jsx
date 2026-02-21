cbf4d9eaad349221ddf395f2bf656ad1617365fa
nrt85 <ananevandrei11@yandex.ru>
2026-02-21 22:51:03 +0100

    init chat for testing

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1>IT IS TESTING APP</h1>
+      </div>
+      <p>
+        Click on Chat on right down place
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Widget from '@hexlet/chatbot-v2';
+import steps from '@hexlet/chatbot-v2/example-steps';
+import '@hexlet/chatbot-v2/styles';
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+    {Widget(steps)}
+  </StrictMode>
+)
